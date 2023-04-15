7bd67cc82807c363df9d19e9d69acf00539f5e74
Putta Khunchalee <ultimaweapon@outlook.com>
2023-04-16 00:02:15 +0700

    Refactors TcpAccept

--- src/windows/tcp.rs
+++ src/windows/tcp.rs
@@ -26,8 +26,8 @@ use windows_sys::Win32::System::IO::CancelIoEx;
 pub struct TcpAccept<'a> {
     iocp: &'a Iocp,
     tcp: &'a mut TcpListener,
-    ct: Option<CancellationToken>,
-    ch: Option<SubscriptionHandle>,
+    cancellation_token: Option<CancellationToken>,
+    cancellation_handle: Option<SubscriptionHandle>,
     pending: Option<Arc<Mutex<AcceptPending>>>,
 }
 
@@ -35,15 +35,142 @@ impl<'a> TcpAccept<'a> {
     pub(crate) fn new(
         iocp: &'a Iocp,
         tcp: &'a mut TcpListener,
-        ct: Option<CancellationToken>,
+        cancellation_token: Option<CancellationToken>,
     ) -> Self {
         Self {
             iocp,
             tcp,
-            ct,
-            ch: None,
+            cancellation_token,
+            cancellation_handle: None,
             pending: None,
         }
+    }
+
+    fn begin_accept(&mut self, cx: &mut Context) -> Result<Arc<Mutex<AcceptPending>>, Error> {
+        // Get address info.
+        let (af, addr_len) = match self.tcp.local_addr() {
+            Ok(v) => {
+                if v.is_ipv6() {
+                    (AF_INET6, size_of::<SOCKADDR_IN6>() as u32)
+                } else {
+                    (AF_INET, size_of::<SOCKADDR_IN>() as u32)
+                }
+            }
+            Err(e) => return Err(e),
+        };
+
+        // Create a client socket.
+        let client = {
+            let s = unsafe { socket(af as _, SOCK_STREAM, IPPROTO_TCP) };
+
+            if s == INVALID_SOCKET {
+                return Err(Error::last_os_error());
+            }
+
+            Socket::new(s)
+        };
+
+        // Construct an overlapped data.
+        let client_sock = client.get();
+        let addr_len = addr_len + 16;
+        let pending = Arc::new(Mutex::new(AcceptPending {
+            client: Some(client),
+            addr_len,
+            result: None,
+        }));
+
+        // Register the socket to IOCP.
+        let server = self.tcp.as_raw_socket() as SOCKET;
+
+        self.iocp.register_handle(server as _)?;
+
+        // Do the accept.
+        let waker = cx.waker().clone();
+        let buf = vec![0u8; (addr_len * 2) as usize];
+        let mut received = 0;
+        let overlapped = Box::into_raw(Overlapped::new(None, buf, waker, Arc::downgrade(&pending)));
+
+        if unsafe {
+            AcceptEx(
+                server,
+                client_sock,
+                (*overlapped).buf() as _,
+                0,
+                addr_len,
+                addr_len,
+                &mut received,
+                overlapped as _,
+            )
+        } == FALSE
+        {
+            let e = Error::last_os_error();
+
+            if e.raw_os_error().unwrap() != ERROR_IO_PENDING as _ {
+                drop(unsafe { Box::from_raw(overlapped) });
+                return Err(e);
+            }
+        }
+
+        Ok(pending)
+    }
+
+    fn end_accept(pending: &mut AcceptPending) -> Result<Option<(TcpStream, SocketAddr)>, Error> {
+        // Check if completed.
+        let result = match pending.result.take() {
+            Some(v) => v,
+            None => return Ok(None),
+        };
+
+        // Get the result.
+        let buf = match result {
+            OverlappedResult::Error(e) => return Err(e),
+            OverlappedResult::Success(buf, _) => buf,
+        };
+
+        // Get the addresses.
+        let mut local = null_mut();
+        let mut local_len = 0;
+        let mut remote = null_mut();
+        let mut remote_len = 0;
+
+        unsafe {
+            GetAcceptExSockaddrs(
+                buf.as_ptr() as _,
+                0,
+                pending.addr_len,
+                pending.addr_len,
+                &mut local,
+                &mut local_len,
+                &mut remote,
+                &mut remote_len,
+            )
+        };
+
+        // Construct a remote address.
+        let (ip, port) = match unsafe { (*remote).sa_family } {
+            AF_INET => unsafe {
+                let addr = remote as *const SOCKADDR_IN;
+                let ip = IpAddr::from(transmute::<_, [u8; 4]>((*addr).sin_addr));
+                let port = u16::from_be((*addr).sin_port);
+
+                (ip, port)
+            },
+            AF_INET6 => unsafe {
+                let addr = remote as *const SOCKADDR_IN6;
+                let ip = IpAddr::from(transmute::<_, [u8; 16]>((*addr).sin6_addr));
+                let port = u16::from_be((*addr).sin6_port);
+
+                (ip, port)
+            },
+            _ => unreachable!("the remote address should always be AF_INET or AF_INET6"),
+        };
+
+        // Get cliet socket.
+        let sock = pending.client.take().unwrap().into_raw();
+        let stream = unsafe { TcpStream::from_raw_socket(sock as _) };
+        let addr = SocketAddr::new(ip, port);
+
+        Ok(Some((stream, addr)))
     }
 }
 
@@ -54,9 +181,9 @@ impl<'a> Future for TcpAccept<'a> {
         let f = self.deref_mut();
 
         // Check if canceled.
-        f.ch = None;
+        f.cancellation_handle = None;
 
-        if let Some(ct) = &f.ct {
+        if let Some(ct) = &f.cancellation_token {
             if ct.is_canceled() {
                 return io_cancel();
             }
@@ -64,142 +191,22 @@ impl<'a> Future for TcpAccept<'a> {
 
         // Check for pending.
         match &f.pending {
-            Some(p) => {
-                // Check if completed.
-                let mut p = p.lock().unwrap();
-
-                if let Some(r) = p.result.take() {
-                    let r = match r {
-                        OverlappedResult::Error(e) => Err(e),
-                        OverlappedResult::Success(buf, _) => unsafe {
-                            // Get addresses.
-                            let mut local = null_mut();
-                            let mut local_len = 0;
-                            let mut remote = null_mut();
-                            let mut remote_len = 0;
-
-                            GetAcceptExSockaddrs(
-                                buf.as_ptr() as _,
-                                0,
-                                p.addr_len,
-                                p.addr_len,
-                                &mut local,
-                                &mut local_len,
-                                &mut remote,
-                                &mut remote_len,
-                            );
-
-                            // Construct a remote address.
-                            let (ip, port) = match (*remote).sa_family {
-                                AF_INET => {
-                                    let a = remote as *mut SOCKADDR_IN;
-
-                                    (
-                                        IpAddr::from(transmute::<_, [u8; 4]>((*a).sin_addr)),
-                                        u16::from_be((*a).sin_port),
-                                    )
-                                }
-                                AF_INET6 => {
-                                    let a = remote as *mut SOCKADDR_IN6;
-
-                                    (
-                                        IpAddr::from(transmute::<_, [u8; 16]>((*a).sin6_addr)),
-                                        u16::from_be((*a).sin6_port),
-                                    )
-                                }
-                                _ => {
-                                    unreachable!("the remote address should be AF_INET or AF_INET6")
-                                }
-                            };
-
-                            // Get cliet socket.
-                            let client = p.client.take().unwrap().into_raw();
-
-                            Ok((
-                                TcpStream::from_raw_socket(client as _),
-                                SocketAddr::new(ip, port),
-                            ))
-                        },
-                    };
-
-                    return Poll::Ready(r);
-                }
-            }
-            None => {
-                // Get address info.
-                let (af, addr_len) = match f.tcp.local_addr() {
-                    Ok(v) => {
-                        if v.is_ipv4() {
-                            (AF_INET, size_of::<SOCKADDR_IN>() as u32)
-                        } else {
-                            (AF_INET6, size_of::<SOCKADDR_IN6>() as u32)
-                        }
-                    }
-                    Err(e) => return Poll::Ready(Err(e)),
-                };
-
-                // Create a client socket.
-                let client = {
-                    let s = unsafe { socket(af as _, SOCK_STREAM, IPPROTO_TCP) };
-
-                    if s == INVALID_SOCKET {
-                        return Poll::Ready(Err(Error::last_os_error()));
-                    }
-
-                    Socket::new(s)
-                };
-
-                // Construct an overlapped data.
-                let cfd = client.get();
-                let addr_len = addr_len + 16;
-                let p = Arc::new(Mutex::new(AcceptPending {
-                    client: Some(client),
-                    addr_len,
-                    result: None,
-                }));
-
-                // Register the socket to IOCP.
-                let sfd = f.tcp.as_raw_socket() as SOCKET;
-
-                if let Err(e) = f.iocp.register_handle(sfd as _) {
-                    return Poll::Ready(Err(e));
-                }
-
-                // Do the accept.
-                let waker = cx.waker().clone();
-                let buf = vec![0u8; (addr_len * 2) as usize];
-                let mut received = 0;
-                let overlapped =
-                    Box::into_raw(Overlapped::new(None, buf, waker, Arc::downgrade(&p)));
-
-                if unsafe {
-                    AcceptEx(
-                        sfd,
-                        cfd,
-                        (*overlapped).buf() as _,
-                        0,
-                        addr_len,
-                        addr_len,
-                        &mut received,
-                        overlapped as _,
-                    )
-                } == FALSE
-                {
-                    let e = Error::last_os_error();
-
-                    if e.raw_os_error().unwrap() != ERROR_IO_PENDING as _ {
-                        drop(unsafe { Box::from_raw(overlapped) });
-                        return Poll::Ready(Err(e));
+            Some(p) => match Self::end_accept(p.lock().unwrap().deref_mut()) {
+                Ok(v) => {
+                    if let Some(v) = v {
+                        return Poll::Ready(Ok(v));
                     }
                 }
-
-                // Keep pending data.
-                f.pending = Some(p);
-            }
+                Err(e) => return Poll::Ready(Err(e)),
+            },
+            None => match f.begin_accept(cx) {
+                Ok(v) => f.pending = Some(v),
+                Err(e) => return Poll::Ready(Err(e)),
+            },
         }
 
         // Watch for cancel.
-        f.ch = watch_cancel(cx, &f.ct);
+        f.cancellation_handle = watch_cancel(cx, &f.cancellation_token);
 
         Poll::Pending
     }
