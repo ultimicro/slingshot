be21ea8f5327ac2ebc2ea25d108a7cb16535e36c
Putta Khunchalee <ultimaweapon@outlook.com>
2023-04-18 22:57:07 +0700

    Adds macOS CI

---
+++ tests/kqueue_cancel.rs
@@ -0,0 +1,6 @@
+use slingshot::bsd::Kqueue;
+
+#[test]
+fn test_kqueue_cancel() {
+    let rt = Kqueue::new();
+}
---
+++ tests/kqueue_tcp.rs
@@ -0,0 +1,6 @@
+use slingshot::bsd::Kqueue;
+
+#[test]
+fn test_kqueue_tcp() {
+    let rt = Kqueue::new();
+}
